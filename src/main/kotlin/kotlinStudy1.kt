b477fbdf92616fe7a5e1ea040013ac799bdb8642
hayaboy <hayaboy@korea.ac.kr>
2023-12-11 10:18:16 +0900

    12_11_kotlin공부1

---
+++ src/main/kotlin/kotlinStudy1.kt
@@ -0,0 +1,56 @@
+//변수
+
+
+//var : 변수값 변경 가능
+//val : 선언시에만 초기화 가능(변경 불가능) -> JAVA의 final
+
+fun main(){
+//    println("hi")
+
+//    var num1:Int = 2   // 숫자형  Byte Short Int Long  Float Double
+//    println(num1)
+//
+//    var num2:Double = 2.6   // 숫자형  Byte Short Int Long  Float Double
+//    println(num2)
+
+//    var ch1 : Char = 'A'
+//    println(ch1)
+//
+//    var ch2 : Char = '가'
+//    println(ch2)
+//
+//    var st : String = "hello"
+//    println(st)
+//
+//    var bl:Boolean = true
+//    println(bl)
+
+
+//    var myName:String
+//    myName="홍길동"
+
+//    var myName="홍길동"
+//
+//    println(myName)
+//    println("안녕하세요 ${myName}님")
+
+
+//    val myName="김유신"
+//    println("안녕하세요 ${myName}님")//
+//    myName="박유신"
+
+    // 데이터 타입?  변수의 값이 null일 수 있다는 것을 표시(?를 표시 하지 않으면 선언시 null이 될 수 없음)
+
+    //var a:Int = null
+    var a:Int? = null
+    print(a)
+
+}
+
+
+//데이터 타입(
+
+//함수
+
+
+//클래스
---
+++ src/main/kotlin/kotlinStudy2.kt
@@ -0,0 +1,21 @@
+fun main(){
+    //형변환
+
+    //코틀린에서는 to변수()를 통해 형변환 가능, 코틀린은 자동 형변환을 지원하지 않음
+
+//    var num1:Int = 123
+//    var st1:String = num1.toString()
+//    println(st1)
+
+//    var num2:Int = 123
+//    var db1:Double = num1.toDouble()
+//    println(db1)
+
+//    var num2:String = "123"
+//    var num3:Int = num2.toInt()
+//    println(num3  + 1)
+
+    var num4:String = "123사"
+    var num5:Int = num4.toInt()
+    println(num5  + 1)
+}
